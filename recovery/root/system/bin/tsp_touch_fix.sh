af1f4f6ff0cc25f29f66a47511e60629189a6513
mrfluffyoven <mrfluffyoven@gmail.com>
2026-06-21 11:21:03 +0000

    Try keep the touchscreen working after the screen sleeps more than once
    Change-Id: I672f64bca150b5327b1daca611f319eadd02ff66

--- recovery/root/system/bin/tsp_touch_fix.sh
+++ recovery/root/system/bin/tsp_touch_fix.sh
@@ -6,10 +6,20 @@ fix() {
 }
 
 dir="/sys/class/backlight/panel/brightness"
+screen_was_off=0
 
 while true; do
-    sleep 0.6
+    sleep 0.3
     new=$(cat "$dir" 2>/dev/null)
-    [ "$old" != "$new" ] && [ "$new" != "0" ] && fix
-    old=$new
+    
+    if [ "$new" = "0" ]; then
+        screen_was_off=1
+    elif [ "$screen_was_off" = "1" ]; then
+        # Screen just came back on from off state
+        sleep 0.2
+        fix
+        sleep 0.3
+        fix  # Second poke for stubborn TSP
+        screen_was_off=0
+    fi
 done
