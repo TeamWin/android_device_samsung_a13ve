048e196c0681fef9a8eb91063902de5165b9ae1b
mrfluffyoven <mrfluffyoven@gmail.com>
2026-06-21 08:13:41 +0000

    check current orangefox flags and attempt to reduce image size
    Change-Id: I2e8c1e519f73a2ab5e8d499ab2821986508815b4

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -26,11 +26,12 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+export TARGET_ARCH=arm64
 export FOX_DISABLE_APP_MANAGER=1
 export FOX_BUILD_TYPE="Beta"
 export FOX_VARIANT=AOSP
+export LC_ALL="C"
 export FOX_NO_SAMSUNG_SPECIAL=1
-export FOX_ENABLE_APP_MANAGER=0
 export FOX_INSTALLER_DEBUG_MODE=1
 export FOX_DELETE_AROMAFM=0
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
@@ -38,6 +39,10 @@ export FOX_RECOVERY_INSTALL_PARTITION=/dev/block/by-name/recovery
 export FOX_RECOVERY_SYSTEM_PARTITION=/dev/block/mapper/system
 export FOX_RECOVERY_VENDOR_PARTITION=/dev/block/mapper/vendor
 export FOX_TARGET_DEVICES="a13ve"
+export FOX_VANILLA_BUILD=1
+export FOX_REMOVE_AAPT=1
+export FOX_REMOVE_BASH=1
+export FOX_DRASTIC_SIZE_REDUCTION=1
 
 # R12 build vars
 export FOX_R12=1
