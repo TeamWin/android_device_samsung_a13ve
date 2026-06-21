3e87f46850cc219253d08e2f7421534fe7be2e96
mrfluffyoven <mrfluffyoven@gmail.com>
2026-06-21 11:57:28 +0000

    Implement touch fix script
    Change-Id: Ib347163922793a35c626c465f2c642961ca8532f

--- recovery/root/system/bin/tsp_touch_fix.sh
+++ recovery/root/system/bin/tsp_touch_fix.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 
 fix() {
-    echo check_connection > /sys/class/sec/tsp/cmd
+    echo fw_update > /sys/class/sec/tsp/cmd
     cat /sys/class/sec/tsp/cmd_result
 }
 
@@ -11,15 +11,12 @@ screen_was_off=0
 while true; do
     sleep 0.3
     new=$(cat "$dir" 2>/dev/null)
-    
+
     if [ "$new" = "0" ]; then
         screen_was_off=1
     elif [ "$screen_was_off" = "1" ]; then
-        # Screen just came back on from off state
-        sleep 0.2
-        fix
         sleep 0.3
-        fix  # Second poke for stubborn TSP
+        fix
         screen_was_off=0
     fi
 done
